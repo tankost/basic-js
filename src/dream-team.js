b1d3ddd962af69d58f7cf220b2913feadc8a8b33
Tatiana Kostukevich <tankostuk@gmail.com>
2022-04-28 15:44:27 +0300

    feat: complete carbon-dating and dream-team tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
+    const names = members.filter(member => typeof member === 'string' ? member : false);
+    const team = names.map(name => {
+        name = name.trim();
+        return name.charAt(0).toUpperCase();
+    })
+    return team.sort().join('');
+  }
+    
 }
 
 module.exports = {

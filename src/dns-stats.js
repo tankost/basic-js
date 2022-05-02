9d7cea287061dd56da46fe14bf8288abcfd98b93
Tatiana Kostukevich <tankostuk@gmail.com>
2022-05-02 04:11:42 +0300

    feat: complete two tasks

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  let allDomains;
+  domains.forEach(domain => {
+      allDomains = domain.split('.');
+      let arr = [];
+      for (let i = allDomains.length - 1; i >= 0; i--) {
+          arr.push(allDomains[i]);
+          let currentDomain = "." + arr.join(".")
+          if (result[currentDomain]) {
+              result[currentDomain]++;
+          } else {
+              result[currentDomain] = 1;
+          }
+      }
+  })
+  return result;
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  options.separator === undefined ? options.separator = '+' : options.separator;
+  options.addition === undefined ? options.addition = '' : String(options.addition);
+  options.additionSeparator === undefined ? options.additionSeparator = '|' : options.additionSeparator;
+  options.repeatTimes === undefined ? options.repeatTimes = 1 : options.repeatTimes;
+  options.additionRepeatTimes === undefined ? options.additionRepeatTimes = 1 : options.additionRepeatTimes;
+
+  let result = '';
+  str = String(str);
+  for (let i = 0; i < options.repeatTimes; i++) {
+      result += str;
+      if (options.addition !== '') {
+          for (let j = 0; j < options.additionRepeatTimes; j++) {
+              result += options.addition;
+              if (options.additionSeparator && (j + 1) < options.additionRepeatTimes) {
+                  result += options.additionSeparator;
+              }
+          }
+      }
+      if (options.separator && (i + 1) < options.repeatTimes) {
+          result += options.separator;
+      }
+  }
+  return result;
 }
 
 module.exports = {
